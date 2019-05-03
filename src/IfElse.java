6389ff0cab5712115704672ad8314919f6e7b95d
Oksana Rudyk <oksrudyk.1999@gmail.com>
2019-05-03 19:14:12 +0300

    Initial commit

---
+++ src/IfElse.java
@@ -0,0 +1,10 @@
+public class IfElse {
+    public static void main(String args[]){
+        String weather = "Raining";
+        if(weather == "Raining"){
+            System.out.println("I am staying home");
+        } else {
+            System.out.println("I am going out for a walk");
+        }
+    }
+}
---
+++ src/взірець.java
@@ -0,0 +1,12 @@
+public class взірець {
+    public static void main(String args[]){
+        int y = 1;
+        int k;
+        for(k = 6; k >= 3; k--)
+            y = y + k;
+        System.out.println("y = " + y);
+        System.out.println("k = " + k);
+
+    }
+
+}
